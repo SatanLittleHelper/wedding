a8ffc1d64bf8b530854ee6eeda774ac50d2ca615
Alexander <a.fenin@unlimit.com>
2024-12-07 17:56:06 +0300

    add section

---
+++ src/app/pages/main/components/section/section.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  imports: [],
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SectionComponent {
+  protected title = input<string>();
+  protected subTitle = input<string>();
+
+}
